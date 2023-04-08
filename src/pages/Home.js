1970e6e02fa8e9fd92f545ce4dcce48afb7e7943
AllanC12 <allandevfront@gmail.com>
2023-04-07 23:04:22 -0300

    criando arquivos iniciais

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+//styles
+import styles from "../sass_pages/Home.scss"
+
+//components
+import Navbar from "../components/Navbar"
+
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home

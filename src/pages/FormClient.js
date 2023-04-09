96cad2d13e40d886274694fbee651a6a6543f2ca
AllanC12 <allandevfront@gmail.com>
2023-04-09 01:17:39 -0300

    animação de form em andamento

--- src/pages/FormClient.js
+++ src/pages/FormClient.js
@@ -1,16 +1,54 @@
 import styles from "./sass_pages/FormClient.module.scss" 
+
 import { Link } from "react-router-dom"
+
+import { useRef , useEffect} from "react"
+
+//img_animation
+import beer from "../images/img_animations/beer.png"
+import cheeseBurguer from "../images/img_animations/cheese-burguer.png"
+import cokeCup from "../images/img_animations/coke_cup.png"
+import dinner from "../images/img_animations/dinner.png"
+import potatoChips from "../images/img_animations/potato_chips.png"
+import sprite from "../images/img_animations/sprite.png"
+
 const FormClient = () => {
+    const divImgsFormAnimation = useRef()
+    let indexImgAnimation = 0;
+
+   const animationForm = () => {
+    
+    const imgsAnimation = divImgsFormAnimation.current.children
+    const arrayImages = Array.from(imgsAnimation)
+
+        setInterval(()=>{
+
+            if(indexImgAnimation === arrayImages.length){
+                indexImgAnimation = 0
+             }
+
+            arrayImages[indexImgAnimation].style.setProperty('opacity','1')
+            indexImgAnimation++
+        },1000)
+   }
+
+
+   useEffect(()=>{
+     animationForm()
+  })
+    
+
   return (
     <div className={styles.banner_main}>
          <form className={styles.form_login}>
              <h2>A fome bateu? Entre aqui e sirva-se</h2>
-            <div className={styles.form_animation}>
-                <img src="../images/img_animations/beer.png" alt="" />
-                {/* <img src="../images/img_animations/" alt="" />
-                <img src="../images/img_animations/" alt="" />
-                <img src="../images/img_animations/" alt="" />
-                <img src="../images/img_animations/" alt="" /> */}
+            <div ref={divImgsFormAnimation}  className={styles.form_animation}>
+                <img src={beer} alt="" />
+                <img src={cheeseBurguer} alt="" />
+                <img src={cokeCup} alt="" />
+                <img src={dinner} alt="" />
+                <img src={potatoChips} alt="" />
+                <img src={sprite} alt="" />
             </div>
              <label>
                   <input type="text" placeholder="Insira seu nome..."/>

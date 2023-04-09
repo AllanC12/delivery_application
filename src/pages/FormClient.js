9b7693f2bdcbaf88043eb58355957944ef825ff3
AllanC12 <allandevfront@gmail.com>
2023-04-08 23:56:22 -0300

    iniciando animação em form de login

--- src/pages/FormClient.js
+++ src/pages/FormClient.js
@@ -5,6 +5,13 @@ const FormClient = () => {
     <div className={styles.banner_main}>
          <form className={styles.form_login}>
              <h2>A fome bateu? Entre aqui e sirva-se</h2>
+            <div className={styles.form_animation}>
+                <img src="../images/img_animations/beer.png" alt="" />
+                {/* <img src="../images/img_animations/" alt="" />
+                <img src="../images/img_animations/" alt="" />
+                <img src="../images/img_animations/" alt="" />
+                <img src="../images/img_animations/" alt="" /> */}
+            </div>
              <label>
                   <input type="text" placeholder="Insira seu nome..."/>
              </label>
@@ -14,7 +21,7 @@ const FormClient = () => {
              </label>
 
              <input className="btn" type="submit" value="Enviar" />
-             
+
              <Link to="/cadastro">Se é novato então clica aqui!</Link>
 
          </form>
---
+++ src/pages/FormNewClient.js
@@ -0,0 +1,11 @@
+import styles from "./sass_pages/FormNewClient.scss"
+
+const FormNewClient = () => {
+  return (
+    <div>
+        <h2>novo cliente </h2>
+    </div>
+  )
+}
+
+export default FormNewClient

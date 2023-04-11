a1fb4fea347759d4a537adf99cbb7b06fc35a4a4
AllanC12 <allandevfront@gmail.com>
2023-04-11 17:46:33 -0300

    iniciando desenvolvimento do banner da home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,15 @@ import styles from "./sass_pages/Home.module.scss"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 
-
 const Home = () => {
   return (
     <div>
       <Navbar/>
+         <div className={styles.banner_home}>
+            <div className={styles.img_banner}></div>
+            <div className={styles.img_banner}></div>
+            <div className={styles.img_banner}></div>
+         </div>
       <Footer/>
     </div>
   )

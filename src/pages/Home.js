69dc990d1d2a280077d3efc65630591772c19538
AllanC12 <allandevfront@gmail.com>
2023-04-12 22:57:21 -0300

    imagens de banner estruturada

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,15 +5,19 @@ import styles from "./sass_pages/Home.module.scss"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 
+import {useFetch} from "../hooks/useFetch"
+
 const Home = () => {
-  return (
+  const url = "http://localhost:3000/foods_banner"
+  const {data:foods_banner,loading} = useFetch(url)
+   return (
     <div>
       <Navbar/>
-         <div className={styles.banner_home}>
-            <div className={styles.img_banner}></div>
-            <div className={styles.img_banner}></div>
-            <div className={styles.img_banner}></div>
-         </div>
+         {foods_banner && foods_banner.map(food_banner => (
+            <div key={food_banner.id} className={styles.banner_home}>
+              <img src={food_banner.adress_image} alt={food_banner.legend}/>
+            </div>
+         ))}
       <Footer/>
     </div>
   )

6970a8be961bb4754f87c7ad50587b3f679d5af6
AllanC12 <allandevfront@gmail.com>
2023-04-16 21:49:38 -0300

    slider da home definido

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,51 +1,57 @@
 //styles
-import styles from "./sass_pages/Home.module.scss"
+import styles from "./sass_pages/Home.module.scss";
 
 //components
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
-import {useFetch} from "../hooks/useFetch"
-import { useEffect, useRef } from "react"
+import { useEffect, useRef } from "react";
 
 const Home = () => {
-  const url = "http://localhost:3000/foods_banner"
-  const {data:foods_banner} = useFetch(url)
+  const imgElement = useRef();
+  const legendBannerElement = useRef()
 
-  const containerBanner = useRef()
-
-
-     useEffect(() => {
-         const imgsBannerElements = containerBanner.current.children
-        const imgElements = Array.from(imgsBannerElements)
-        let indexSlide = 0;
-        let indexMax = imgElements.length - 1
+  const imagesConfig = [
+    {id: 1,
+     legend:"Pizzas maravilhosas...",
+    address: "https://pastapizza.com.br/wp-content/uploads/2017/07/Pizza-Pizzaria-Forno-Forza-Express-1536x1007.jpg"},
+    {id: 2,
+    legend:"...Sobremesas impactantes...",
+    address:"https://www.sabornamesa.com.br/media/k2/items/cache/7d2898c3630feea92ec1553d16389ff6_XL.jpg"},
+    {id: 3,
+      legend:"Drinks de tirar o fôlego...",
+     address:"https://blog.ginbrasil.com.br/wp-content/uploads/2020/08/Gin-Brasil-11-Entenda-a-fun%C3%A7%C3%A3o-do-gelo-no-seu-drink-e-por-que-ele-%C3%A9-importante.jpg"
+    }
+  ];
   
-        if(indexSlide === indexMax)
-          indexSlide = 0
-    
-        imgElements[indexSlide].styles.setProperty("opacity","0")
-         indexSlide++
-        imgElements[indexSlide].styles.setProperty("opacity","1")
-     })   
- 
 
-   return (
+  useEffect(()=>{
+    const imgBanner = imgElement.current
+    const imgLegend = legendBannerElement.current
+    let index = 0
+    let indexMax = imagesConfig.length - 1
+    setInterval(()=>{
+      if(index > indexMax)
+        index = 0
+      imgBanner.setAttribute("src",imagesConfig[index].address)
+      imgLegend.innerText = imagesConfig[index].legend
+      index++
+    },5000)
+  },[])
+
+
+  return (
     <div>
-      <Navbar/>
-      <div ref={containerBanner} className={styles.banner_home}>
-         {foods_banner && foods_banner.map(food_banner => (
-           <div key={food_banner.adress_image} className={styles.banner_wraper} id="selected">
-              <h2>{food_banner.legend}</h2>
-              <img src={food_banner.adress_image} alt={food_banner.legend}/>
-           </div>
-            
-         ))}
+      <Navbar />
+      <div  className="banner_home">
+        <div className="banner_wraper">
+          <h2 ref={legendBannerElement}>{imagesConfig[0].legend}</h2>
+          <img ref={imgElement} src={imagesConfig[0].address} />
+        </div>
       </div>
-
-      <Footer/>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

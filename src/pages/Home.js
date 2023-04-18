4ae70e01a27d43b52f2914fc70eec64fd99572f2
AllanC12 <allandevfront@gmail.com>
2023-04-17 22:38:44 -0300

    atualização de slide

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,44 +9,44 @@ import { useEffect, useRef } from "react";
 
 const Home = () => {
   const imgElement = useRef();
-  const legendBannerElement = useRef()
 
-  const imagesConfig = [
-    {id: 1,
-     legend:"Pizzas maravilhosas...",
-    address: "https://pastapizza.com.br/wp-content/uploads/2017/07/Pizza-Pizzaria-Forno-Forza-Express-1536x1007.jpg"},
-    {id: 2,
-    legend:"...Sobremesas impactantes...",
-    address:"https://www.sabornamesa.com.br/media/k2/items/cache/7d2898c3630feea92ec1553d16389ff6_XL.jpg"},
-    {id: 3,
-      legend:"Drinks de tirar o fôlego...",
-     address:"https://blog.ginbrasil.com.br/wp-content/uploads/2020/08/Gin-Brasil-11-Entenda-a-fun%C3%A7%C3%A3o-do-gelo-no-seu-drink-e-por-que-ele-%C3%A9-importante.jpg"
-    }
-  ];
-  
+  useEffect(() => {
+    const imgBanner = imgElement.current.children;
+    let index = 0;
+    let indexMax = imgBanner.length - 1;
 
-  useEffect(()=>{
-    const imgBanner = imgElement.current
-    const imgLegend = legendBannerElement.current
-    let index = 0
-    let indexMax = imagesConfig.length - 1
-    setInterval(()=>{
-      if(index > indexMax)
-        index = 0
-      imgBanner.setAttribute("src",imagesConfig[index].address)
-      imgLegend.innerText = imagesConfig[index].legend
-      index++
-    },5000)
-  },[])
-
+    setInterval(() => {
+      imgBanner[index].classList.remove("selected");
+      index++;
+      if (index > indexMax) index = 0;
+      imgBanner[index].classList.add("selected");
+    }, 4000);
+  }, []);
 
   return (
     <div>
       <Navbar />
-      <div  className="banner_home">
+      <div ref={imgElement} className="banner_home">
+        <div className="banner_wraper selected">
+          <h2 className="legend_banner">"Pizzas maravilhosas..."</h2>
+          <img
+            className="img_banner"
+            src="https://pastapizza.com.br/wp-content/uploads/2017/07/Pizza-Pizzaria-Forno-Forza-Express-1536x1007.jpg"
+          />
+        </div>
         <div className="banner_wraper">
-          <h2 ref={legendBannerElement}>{imagesConfig[0].legend}</h2>
-          <img ref={imgElement} src={imagesConfig[0].address} />
+          <h2 className="legend_banner">...Sobremesas impactantes...</h2>
+          <img
+            className="img_banner"
+            src="https://www.sabornamesa.com.br/media/k2/items/cache/7d2898c3630feea92ec1553d16389ff6_XL.jpg"
+          />
+        </div>
+        <div className="banner_wraper">
+          <h2 className="legend_banner">Drinks de tirar o fôlego...</h2>
+          <img
+            className="img_banner"
+            src="https://blog.ginbrasil.com.br/wp-content/uploads/2020/08/Gin-Brasil-11-Entenda-a-fun%C3%A7%C3%A3o-do-gelo-no-seu-drink-e-por-que-ele-%C3%A9-importante.jpg"
+          />
         </div>
       </div>
       <Footer />

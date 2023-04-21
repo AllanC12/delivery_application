5dd7a584157500638ffdad3cb2ea181f15ef2649
AllanC12 <allandevfront@gmail.com>
2023-04-20 22:44:22 -0300

    corrigr e aperfeiçoar login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
- import './App.css';
+import "./App.css";
 //hooks
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
- //pages
-import Home from "./pages/Home.js"
-import FormClient from './pages/FormClient';
-import FormNewClient from './pages/FormNewClient';
-import Menu from './pages/Menu';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useFetch } from "./hooks/useFetch";
+
+//pages
+import Home from "./pages/Home.js";
+import FormClient from "./pages/FormClient";
+import FormNewClient from "./pages/FormNewClient";
+import Menu from "./pages/Menu";
 
 function App() {
+  const [nameClient, setNameClient] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const urlDataClient = `http://localhost:3000/clients`;
+  const { data: clients, loading } = useFetch(urlDataClient);
+
+  console.log(clients);
+
+  const userNameValidate = clients.filter(
+    (client) => client.name === nameClient
+  );
+
+  const userPasswordValidate = clients.filter(
+    (client) => client.password === password
+  );
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    if (userNameValidate.length > 0) {
+      if (userPasswordValidate.length > 0) {
+        setSuccessMessage(`Seja bem vindo ${nameClient}`);
+      } else {
+        setErrorMessage(`Senha incorreta`);
+        return;
+      }
+    } else {
+      setErrorMessage(`Usuário não encontrado...`);
+      return;
+    }
+
+    setNameClient("");
+    setPassword("");
+    setInterval(() => {
+      window.location.href = "http://localhost:3001/inicio";
+    }, 700);
+  };
+
   return (
     <div className="App">
-        <BrowserRouter>
-           <Routes>
-              <Route path="/" element={<FormClient/>}/>
-              <Route path="/cadastro" element={<FormNewClient/>}/>
-              <Route path="/inicio" element={<Home/>}/>
-              <Route path="/cardapio" element={<Menu/>}/>
-           </Routes>
-        </BrowserRouter>
-     </div>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <FormClient
+                nameClient={nameClient}
+                password={password}
+                setNameClient={setNameClient}
+                setPassword={setPassword}
+                handleLogin={handleLogin}
+                errorMessage={errorMessage}
+                successMessage={successMessage}
+                setErrorMessage={setErrorMessage}
+                loading={loading}
+                clients={clients}
+              />
+            }
+          />
+          <Route path="/cadastro" element={<FormNewClient />} />
+          <Route
+            path="/inicio"
+            element={userPasswordValidate ? <Home /> : <FormNewClient />}
+          />
+          <Route path="/cardapio" element={<Menu />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/pages/FormClient.js
+++ src/pages/FormClient.js
@@ -1,133 +1,111 @@
-import styles from "./sass_pages/FormClient.module.scss" 
+import styles from "./sass_pages/FormClient.module.scss";
 
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 //hooks
-import { useRef , useEffect, useState} from "react"
-import { useNavigate } from "react-router-dom"
-import { useFetch } from "../hooks/useFetch"
+import { useRef, useEffect, useState } from "react";
+
 
 //img_animation
-import beer from "../images/img_animations/beer.png"
-import cheeseBurguer from "../images/img_animations/cheese-burguer.png"
-import cokeCup from "../images/img_animations/coke_cup.png"
-import dinner from "../images/img_animations/dinner.png"
-import potatoChips from "../images/img_animations/potato_chips.png"
-import sprite from "../images/img_animations/sprite.png"
+import beer from "../images/img_animations/beer.png";
+import cheeseBurguer from "../images/img_animations/cheese-burguer.png";
+import cokeCup from "../images/img_animations/coke_cup.png";
+import dinner from "../images/img_animations/dinner.png";
+import potatoChips from "../images/img_animations/potato_chips.png";
+import sprite from "../images/img_animations/sprite.png";
 
-const FormClient = () => {
-    const [name,setName] = useState('')
-    const [password,setPassword] = useState('')
+const FormClient = ({
+  nameClient,
+  password,
+  handleLogin,
+  setNameClient,
+  setPassword,
+  errorMessage,
+  successMessage,
+  setErrorMessage,
+  loading,
+}) => {
+  const divImgsFormAnimation = useRef();
+  let indexImgAnimation = 0;
 
-    const urlDataClient = `http://localhost:3000/clients`
-    const {data:clients,loading} = useFetch(urlDataClient)
-    const [errorMessage,setErrorMessage] = useState('')
-    const [successMessage,setSuccessMessage] = useState('')
-    const Navigate = useNavigate()
+  const animationForm = () => {
+    const imgsAnimation = divImgsFormAnimation.current.children;
+    const arrayImages = Array.from(imgsAnimation);
 
-    const divImgsFormAnimation = useRef()
-    let indexImgAnimation = 0;
-    
-   const animationForm = () => {
-    
-    const imgsAnimation = divImgsFormAnimation.current.children
-    const arrayImages = Array.from(imgsAnimation)
-
-        setInterval(()=>{
-
-            if(indexImgAnimation === arrayImages.length){
-                indexImgAnimation = 0
-                for(let i = 0; i < arrayImages.length; i++){
-                    arrayImages[i].style.setProperty('opacity','0')
-                }
-             }
-
-            arrayImages[indexImgAnimation].style.setProperty('opacity','1')
-            indexImgAnimation++
-        
-        },1000)
-   }
-
-
-   useEffect(()=>{
-     animationForm()
-  },[])
-    
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-     const userNameValidate = clients.filter(client => (
-        client.name === name
-    ))
-
-    const userPasswordValidate = clients.filter(client => (
-        client.password === password
-    ))
-
-    if(userNameValidate.length > 0){
-        if(userPasswordValidate.length > 0){
-            setSuccessMessage(`Seja bem vindo ${name}`)
-        }else{
-            setErrorMessage(`Senha incorreta`)
-            return
+    setInterval(() => {
+      if (indexImgAnimation === arrayImages.length) {
+        indexImgAnimation = 0;
+        for (let i = 0; i < arrayImages.length; i++) {
+          arrayImages[i].style.setProperty("opacity", "0");
         }
-    }else{
-        setErrorMessage(`Usuário não encontrado...`)
-        return
-    }
+      }
 
-    setName('')
-    setPassword('')
-    setInterval(()=>{  
-        Navigate('/inicio')
-    },700)
-  }
+      arrayImages[indexImgAnimation].style.setProperty("opacity", "1");
+      indexImgAnimation++;
+    }, 1000);
+  };
+
+  useEffect(() => {
+    animationForm();
+  }, []);
 
   return (
     <div className={styles.banner_form_login}>
-         <form onSubmit={handleSubmit} className={styles.form_login}>
-             <h2>A fome bateu? Entre aqui e sirva-se</h2>
-            <div ref={divImgsFormAnimation}  className={styles.form_animation}>
-                <img src={beer} alt="beer" />
-                <img src={cheeseBurguer} alt="cheeseBurguer" />
-                <img src={cokeCup} alt="cokeCup" />
-                <img src={dinner} alt="dinner" />
-                <img src={potatoChips} alt="potatoChips" />
-                <img src={sprite} alt="sprite" />
-            </div>
-             <label >
-                  <input onFocus={()=> setErrorMessage("")} type="text" 
-                  placeholder="Insira seu nome de usuário..."
-                  value={name}  
-                  onChange={(e)=> setName(e.target.value)}/>
-             </label>
+      <form onSubmit={handleLogin} className={styles.form_login}>
+        <h2>A fome bateu? Entre aqui e sirva-se</h2>
+        <div ref={divImgsFormAnimation} className={styles.form_animation}>
+          <img src={beer} alt="beer" />
+          <img src={cheeseBurguer} alt="cheeseBurguer" />
+          <img src={cokeCup} alt="cokeCup" />
+          <img src={dinner} alt="dinner" />
+          <img src={potatoChips} alt="potatoChips" />
+          <img src={sprite} alt="sprite" />
+        </div>
+        <label>
+          <input
+            onFocus={() => setErrorMessage("")}
+            type="text"
+            placeholder="Insira seu nome de usuário..."
+            value={nameClient}
+            onChange={(e) => setNameClient(e.target.value)}
+          />
+        </label>
 
-             <label>
-                  <input onFocus={()=> setErrorMessage("")} type="password" 
-                  placeholder="Insira sua senha..."
-                  value={password} 
-                  onChange={(e)=> setPassword(e.target.value)}/>
-             </label>
+        <label>
+          <input
+            onFocus={() => setErrorMessage("")}
+            type="password"
+            placeholder="Insira sua senha..."
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
 
-            {!loading && <input className="btn btn-login disabled" type="submit" value="Entrar" />}
+        {!loading && (
+          <input
+            className="btn btn-login disabled"
+            type="submit"
+            value="Entrar"
+          />
+        )}
 
-            {loading && 
-               <input disabled className="btn btn-login" type="submit" value="Entrando" />
-            }
+        {loading && (
+          <input
+            disabled
+            className="btn btn-login"
+            type="submit"
+            value="Entrando"
+          />
+        )}
 
-             {errorMessage && (
-                <p className="message-error">{errorMessage}</p>
-             )}
+        {errorMessage && <p className="message-error">{errorMessage}</p>}
 
-             {successMessage &&(
-                <p className="message-success">{successMessage}</p>
-             )}
+        {successMessage && <p className="message-success">{successMessage}</p>}
 
-             <Link to="/cadastro">Se é novato então clica aqui!</Link>
+        <Link to="/cadastro">Se é novato então clica aqui!</Link>
+      </form>
+    </div>
+  );
+};
 
-         </form>
-     </div>
-  )
-}
-
-export default FormClient
+export default FormClient;

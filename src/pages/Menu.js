cac6e555d57847fae8c4f18730fb05d0b4d9a313
AllanC12 <allandevfront@gmail.com>
2023-04-23 00:02:24 -0300

    referencias para elementos de pagina criados

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,5 +1,7 @@
 import styles from "./sass_pages/Menu.module.scss";
 
+import { useRef,useEffect } from "react";
+
 //components
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -7,6 +9,20 @@ import before_page from "../images/arrows/before-page.png"
 import next_page from "../images/arrows/next-page.png"
 
 const Menu = () => {
+  const pageOne = useRef();
+  const pageTwo = useRef();
+  const pageThree = useRef();
+  const pageFour = useRef();
+
+  const beforePage = () => {
+
+ }
+
+ const nextPage = () => {
+   pageTwo.style.setProperty("")
+ }
+
+
   return (
     <div className={styles.menu_element}>
       <Navbar />
@@ -14,13 +30,14 @@ const Menu = () => {
       <div className={styles.banner_menu}>
         <img src="https://www.emporiotambo.com.br/pub/media/resized/1300x800/ves/blog/xguia-de-mesa.jpg.pagespeed.ic.M976sIeg6W.jpg" />
         <div className={styles.menu}>
-          <img src={before_page}/>
+          <img onClick={beforePage} src={before_page}/>
           <div className={styles.cover_menu}>
-            <div className={`${styles.page_one} ${styles.page}`}></div>
-            <div className={`${styles.page_two} ${styles.page}`}></div>
-            <div className={`${styles.page_three} ${styles.page}`}></div>
+            <div ref={pageOne} className={`${styles.page_one} ${styles.page}`}></div>
+            <div ref={pageTwo} className={`${styles.page_two} ${styles.page}`}></div>
+            <div ref={pageThree} className={`${styles.page_three} ${styles.page}`}></div>
+            <div ref={pageFour} className={`${styles.page_four} ${styles.page}`}></div>
           </div>
-          <img src={next_page}/>
+          <img onClick={nextPage} src={next_page}/>
         </div>
       </div>
 

3925653fda920164c9a987d3420cabc7ff374061
AllanC12 <allandevfront@gmail.com>
2023-04-22 23:44:31 -0300

    css do componente de cardapio implementado

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -3,6 +3,8 @@ import styles from "./sass_pages/Menu.module.scss";
 //components
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import before_page from "../images/arrows/before-page.png"
+import next_page from "../images/arrows/next-page.png"
 
 const Menu = () => {
   return (
@@ -12,11 +14,13 @@ const Menu = () => {
       <div className={styles.banner_menu}>
         <img src="https://www.emporiotambo.com.br/pub/media/resized/1300x800/ves/blog/xguia-de-mesa.jpg.pagespeed.ic.M976sIeg6W.jpg" />
         <div className={styles.menu}>
+          <img src={before_page}/>
           <div className={styles.cover_menu}>
             <div className={`${styles.page_one} ${styles.page}`}></div>
             <div className={`${styles.page_two} ${styles.page}`}></div>
             <div className={`${styles.page_three} ${styles.page}`}></div>
           </div>
+          <img src={next_page}/>
         </div>
       </div>
 

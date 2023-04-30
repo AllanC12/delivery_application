b2e4a0d1415eab412c313ea6105bf3d2f0ced8f9
AllanC12 <allandevfront@gmail.com>
2023-04-30 01:02:20 -0300

    função de remover pedidos adicionada

--- src/components/ModalOrder.js
+++ src/components/ModalOrder.js
@@ -5,7 +5,7 @@ import { useState,useRef, useEffect } from "react";
 import { FaWindowClose } from "react-icons/fa";
 import { MdRemoveCircle } from "react-icons/md"
 
-const ModalOrder = ({ orders }) => {
+const ModalOrder = ({ orders,setOrders }) => {
   const modalRef = useRef();
   const [valueTotalOrder,setValueTotalOrder] = useState("")
 
@@ -24,6 +24,11 @@ const ModalOrder = ({ orders }) => {
 useEffect(()=>{
   sumValueOrder()
 },[orders])
+
+const removeOrder = (id) => {
+  const ordersFiltered = orders.filter(order => order.id !== id)
+  setOrders(ordersFiltered)
+}
 
 
   const handleModal = (modal) => {
@@ -50,7 +55,7 @@ useEffect(()=>{
                 <div className={styles.order} key={order.id}>
                   <p>{order.name}</p>
                   <p>R${order.price}</p>
-                  <MdRemoveCircle onClick={removeOrder} className={styles.remove_order}/>
+                  <MdRemoveCircle onClick={()=>removeOrder(order.id)} className={styles.remove_order}/>
                 </div>
               ))}
           </div>

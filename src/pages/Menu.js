f1f3a4fcfc85010485bbf6ee61ea2602f6957a07
AllanC12 <allandevfront@gmail.com>
2023-04-29 23:32:03 -0300

    implementando função de remover pedidos

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -36,8 +36,7 @@ const Menu = () => {
   const { data: desserts } = useFetch(urlsForMenu[3]);
   const { data: drinks_alcool } = useFetch(urlsForMenu[4]);
 
-  const [orders,setOrders] = useState([
-   ])
+  const [orders,setOrders] = useState([])
    
   const verifyPageTwo = (pageTwo, pageFour, pageOne, pageThree) => {
     if (

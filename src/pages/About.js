7949afe0d5ef79e65e2155f9d62326caffeb707d
AllanC12 <allandevfront@gmail.com>
2023-05-03 21:57:23 -0300

    iniciando estrutura de página About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import Navbar from "../components/Navbar.js"
 
 const About = () => {
+  const adressBanner = "https://vinicolaaraucaria.com.br/wp-content/uploads/2020/06/interna_restaurante-1-1400x700.jpg"
   return (
-    <div>About</div>
+    <div>
+       <Navbar/>
+       <main>
+          <img src={adressBanner}/>
+       </main>
+    </div>
   )
 }
 

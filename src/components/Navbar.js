bea1076fad1117773f50b5e4a54a3056d89a4626
AllanC12 <allandevfront@gmail.com>
2023-05-05 22:47:01 -0300

    css de menu do cliente estruturado

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,16 @@ import { NavLink } from 'react-router-dom'
 
 import logo from "../images/img_animations/logo.png"
 
+import { useRef } from 'react'
+
 const Navbar = () => {
+  const profileRef = useRef()
+  
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>
          <img src={logo} alt="logo" />
-      </div>
+       </div>
       <div className={styles.menu_desktop}>
          <ul>
            <li><NavLink to="/inicio">Início</NavLink></li>
@@ -18,6 +22,20 @@ const Navbar = () => {
            <li><NavLink to="/contatos">Contatos</NavLink></li>
          </ul>
       </div>
+
+      <div className={styles.about_client}>
+         <div ref={profileRef} className={styles.profile_client}>
+            
+         </div>
+          <nav className={styles.menu_data_client}>
+            <ul>
+               <li><NavLink to="/meu_perfil">Meu perfil</NavLink></li>
+               <li><NavLink to="meus_dados">Meus dados</NavLink></li>
+               <li><NavLink to="/">Sair</NavLink></li>
+            </ul>
+          </nav>
+      </div>
+
     </nav>
   )
 }

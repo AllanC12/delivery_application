3817d755905334d209654a0c3281a9703b99d0fa
AllanC12 <allandevfront@gmail.com>
2023-05-21 13:52:38 -0300

    mudando chamada de banners pro scss

---
+++ src/pages/Contacts.js
@@ -0,0 +1,21 @@
+import  "./sass_pages/Contacts.scss"
+
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+
+const Contacts = () => {
+  return (
+    <div className="contacts">
+      <Navbar/>
+        <div className="contacts_banner">
+          <div className="box_contacts">
+
+          </div>
+        </div>
+      <Footer/>
+    </div>
+
+  )
+}
+
+export default Contacts

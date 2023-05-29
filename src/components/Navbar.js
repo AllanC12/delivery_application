ba51ab20237d4f93894a2a083451c921f9959c15
AllanC12 <allandevfront@gmail.com>
2023-05-28 22:31:48 -0300

    refatotando navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,22 +5,24 @@ import { NavLink } from "react-router-dom";
 import logo from "../images/img_animations/logo.png";
 
 import { FaUser, FaWhatsapp, FaEnvelope } from "react-icons/fa";
-import {FiMenu} from "react-icons/fi"
+import { FiMenu } from "react-icons/fi";
+import { GrClose } from "react-icons/gr";
 
 import { ContextUserData } from "../context/ContextUser";
 import { useContext, useRef } from "react";
 
 const Navbar = () => {
   const userDataContext = useContext(ContextUserData);
-  const imageUserProfile =
-    userDataContext.value.confirmUser.userValidate[0].urlImage;
+  const imageUserProfile = userDataContext.value.confirmUser.userValidate[0].urlImage;
 
+  const navbarRef = useRef()
   const profilePhotoRef = useRef();
   const menuContactRef = useRef();
   const menuClientRef = useRef();
   const linkContactRef = useRef();
-  const iconMenuMobileRef = useRef()
-  const menuMobileRef = useRef()
+  const iconMenuMobileRef = useRef();
+  const menuMobileRef = useRef();
+  let visibilityMenu = false;
 
   const handleMenuClient = (target) => {
     const profilePhoto = profilePhotoRef.current;
@@ -28,68 +30,32 @@ const Navbar = () => {
     const menuClient = menuClientRef.current;
     const linkContact = linkContactRef.current;
 
-    if (target === profilePhoto) {
-      menuClient.style.setProperty("display", "block");
-      console.log("perfil")
+    if (target === profilePhoto) menuClient.style.setProperty("display", "block");
+    if (target === linkContact) menuContact.style.setProperty("display", "block");
 
-    } else if (target === linkContact) {
-      menuContact.style.setProperty("display", "block");
-      console.log("contato")
-     } else {
-      menuContact.style.setProperty("display", "none");
-      menuClient.style.setProperty("display", "none");
+
+   };
+
+  const handleMenuMobile = (target) => {
+    const iconMenu = iconMenuMobileRef.current;
+    const menuMobile = menuMobileRef.current;
+
+    if (target === iconMenu) {
+      menuMobile.style.setProperty("right", "0");
+      console.log("aqui");
+      //  visibilityMenu = true
     }
   };
 
-  const handleMenuMobile = (target) =>{
-    let statusVisiblity = false
-    const iconMenu = iconMenuMobileRef.current
-    const menuMobile = menuMobileRef.current
-
-    // if(target === iconMenu){
-      menuMobile.style.setProperty("right","0")
-       statusVisiblity = true
-    // }
-  }
-
   return (
-    <nav className="navbar" onMouseLeave={(e) => handleMenuClient(e.target)}>
-
+    <nav ref={navbarRef} className="navbar" >
       <div className="logo">
         <img src={logo} alt="logo" />
       </div>
 
-      <div className="menu_desktop">
-        <ul>
-          <li>
-            <NavLink to="/inicio">Início</NavLink>
-          </li>
-          <li>
-            <NavLink to="/cardapio">Cardápio</NavLink>
-          </li>
-          <li>
-            <NavLink to="/sobre">Nossa história</NavLink>
-          </li>
-
-          <li>
-            <span
-              onMouseEnter={(e) => handleMenuClient(e.target)}
-              ref={linkContactRef}
-            >
-              Fale com a gente
-            </span>
-          </li>
-        </ul>
-      </div>
-
-
-      <div className="menu_mobile">
-        <div  ref={iconMenuMobileRef} onClick={(e) => handleMenuMobile(e.target)} className="icon_menu">
-          <FiMenu /> 
-        </div>
-         
-      <nav ref={menuMobileRef}>
-        <ul>
+      <div className="container_menu">
+        <div className="menu_desktop">
+          <ul>
             <li>
               <NavLink to="/inicio">Início</NavLink>
             </li>
@@ -100,61 +66,86 @@ const Navbar = () => {
               <NavLink to="/sobre">Nossa história</NavLink>
             </li>
 
-            <li>
-              <span
+            <li ref={linkContactRef} onMouseEnter={(e) => handleMenuClient(e.target)}>
+              Fale com a gente
 
-              >
-                Fale com a gente
-              </span>
+              <div ref={menuContactRef}  onMouseLeave={(e) => handleMenuClient(e.target)} className="menu_contact menu">
+
+                <div className="container_contact">
+                  <FaWhatsapp />
+                  <a target="_blank" href="http://wa.me/37988551832">
+                    Whataspp
+                  </a>
+                </div>
+
+                <div className="container_contact">
+                  <FaEnvelope />
+                  <a target="_blank" href="mailto:allandevfront@gmail.com">
+                    Email
+                  </a>
+                </div>
+
+              </div>
             </li>
-          </ul>
-      </nav>
 
+          </ul>
+        </div>
 
       </div>
 
       <div className="about_client">
-        <div
-          onMouseEnter={(e) => handleMenuClient(e.target)}
-          className="profile_client"
-        >
+        
+        <div ref={profilePhotoRef} className="photo_client">
           {imageUserProfile !== "" ? (
-            <img  ref={profilePhotoRef}
-            src={imageUserProfile} />
+            <img ref={profilePhotoRef} src={imageUserProfile} />
           ) : (
             <FaUser />
           )}
         </div>
 
-        <nav ref={menuContactRef} className="menu_contact menu">
-          <ul>
-            <li>
-              <FaWhatsapp />
-              <a target="_blank" href="http://wa.me/37988551832">
-                Whataspp
-              </a>
-            </li>
+        <div ref={menuClientRef} className="menu_data_client menu">
+           <ul>
+              <li>
+                <NavLink to="/meus_dados">Meus dados</NavLink>
+              </li>
+              <li>
+                <NavLink to="/">Sair</NavLink>
+              </li>
+            </ul>
+        </div>
 
-            <li>
-              <FaEnvelope />
-              <a target="_blank" href="mailto:allandevfront@gmail.com">
-                Email
-              </a>
-            </li>
-          </ul>
-        </nav>
-
-        <nav ref={menuClientRef} className="menu_data_client menu">
-          <ul>
-            <li>
-              <NavLink to="/meus_dados">Meus dados</NavLink>
-            </li>
-            <li>
-              <NavLink to="/">Sair</NavLink>
-            </li>
-          </ul>
-        </nav>
       </div>
+
+      {/* <div className="menu_mobile">
+        <div  ref={iconMenuMobileRef} onClick={(e) => handleMenuMobile(e.target)} className="icon_menu">
+          {!visibilityMenu ? <FiMenu /> : <GrClose/>}           
+        </div>
+         
+        <nav ref={menuMobileRef}>
+          <ul>
+              <li>
+                <NavLink to="/inicio">Início</NavLink>
+              </li>
+              <li>
+                <NavLink to="/cardapio">Cardápio</NavLink>
+              </li>
+              <li>
+                <NavLink to="/sobre">Nossa história</NavLink>
+              </li>
+
+              <li>
+                   Fale com a gente 
+               </li>
+            </ul>
+      </nav>
+
+
+      </div> */}
+
+      {/* <nav ref={menuClientRef} className="menu_data_client menu">
+
+        </nav>
+      </div> */}
     </nav>
   );
 };
